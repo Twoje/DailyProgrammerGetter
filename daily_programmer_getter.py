1f2821d0b9169455d760bd1026aa947ff16d1087
Luke Mommaerts <lmommaerts@gmail.com>
2013-09-13 16:19:38 -0400

    Added Challenge, Date, and Difficulty

--- daily_programmer_getter.py
+++ daily_programmer_getter.py
@@ -1,4 +1,5 @@
 import praw
+import re
 import gdata.spreadsheet.service
 
 r = praw.Reddit(user_agent='my_cool_application')
@@ -19,6 +20,38 @@ ss_client.password = password
 ss_client.source = 'Daily Programmer to Spreadsheet'
 ss_client.ProgrammaticLogin()
 
+def get_title_data(full_title):
+  title_data = {}
+
+  # Get Date
+  date_match = re.match(r"\[\d+/\d+/\d+\]", full_title)
+  if not date_match:
+    return False
+
+  title_data['date'] = date_match.group(0)[1:-1]
+  trunc_title = full_title[len(title_data['date']) + 3:]
+
+  # Get Challenge number
+  challenge_match = re.match(r"\bchallenge\b #\d+", trunc_title, re.IGNORECASE)
+  if not challenge_match:
+    return False
+
+  title_data['challenge'] = challenge_match.group(0)
+  trunc_title = trunc_title[len(title_data['challenge']) + 1:]
+
+  print trunc_title
+
+  difficulty_match = re.match(r"\[\w+\]", trunc_title)
+  if difficulty_match:
+    title_data['difficulty'] = difficulty_match.group(0)[1:-1]
+  else:
+    title_data['difficulty'] = "N/A"
+  trunc_title = trunc_title[len(title_data['difficulty']) + 3:]
+
+  title_data['title'] = trunc_title
+
+  return title_data
+
 def insert_row(data):
   entry = ss_client.InsertRow(data, spreadsheet_key, worksheet_id)
   if isinstance(entry, gdata.spreadsheet.SpreadsheetsList):
@@ -28,6 +61,16 @@ def insert_row(data):
 
 for submission in submissions:
   data = {}
-  data['title'] = submission.title
+  full_title = submission.title
+
+  title_data = get_title_data(full_title)
+
+  if not title_data:
+    continue
+
+  data['challenge'] = title_data['challenge']
+  data['date'] = title_data['date']
+  data['title'] = title_data['title']
+  data['difficulty'] = title_data['difficulty']
   data['content'] = submission.selftext
   insert_row(data)
